eb7da2d6ab2df07e5c4dd99547da316afb0c452a
Samuel Cabezas <samuel.cabezas.632@gmail.com>
2019-09-20 00:18:19 -0300

    Añadir patron singleton para Provider

--- lib/src/blocs/provider.dart
+++ lib/src/blocs/provider.dart
@@ -5,10 +5,20 @@ export 'package:flutter_form_validation/src/blocs/login_bloc.dart';
 
 class Provider extends InheritedWidget{
 
-  final loginBloc = LoginBloc();
+  static Provider _instancia;
 
-  Provider({Key key, Widget child})
+  factory Provider({Key key, Widget child}) {
+    if (_instancia == null) {
+      _instancia = new Provider._internal(key: key, child: child);
+    }
+
+    return _instancia;
+  }
+
+  Provider._internal({Key key, Widget child})
     : super(key: key, child: child);
+  
+  final loginBloc = LoginBloc();
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
