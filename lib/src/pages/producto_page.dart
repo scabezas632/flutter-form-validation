d2671b41af9a7cb66f45d5efa399fa33da59ea84
Samuel Cabezas <samuel.cabezas.632@gmail.com>
2019-09-20 18:15:34 -0300

    Añadir validación de formulario para la pantalla de productos

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_validation/src/models/producto_model.dart';
+import 'package:flutter_form_validation/src/utils/utils.dart' as utils;
 
-class ProductoPage extends StatelessWidget {
-  const ProductoPage({Key key}) : super(key: key);
+class ProductoPage extends StatefulWidget {
+  @override
+  _ProductoPageState createState() => _ProductoPageState();
+}
+
+class _ProductoPageState extends State<ProductoPage> {
+  final formKey = GlobalKey<FormState>();
+
+  ProductoModel producto = new ProductoModel();
 
   @override
   Widget build(BuildContext context) {
@@ -22,10 +31,12 @@ class ProductoPage extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.all(15.0),
         child: Form(
+          key: formKey,
           child: Column(
             children: <Widget>[
               _crearNombre(),
               _crearPrecio(),
+              _crearDisponible(context),
               _crearBoton(context),
             ],
           ),
@@ -36,19 +47,35 @@ class ProductoPage extends StatelessWidget {
 
   Widget _crearNombre() {
     return TextFormField(
+      initialValue: producto.titulo,
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
         labelText: 'Producto'
       ),
+      onSaved: (value) => producto.titulo = value,
+      validator: (value) {
+        if (value.length < 3) {
+          return 'Ingrese el nombre del producto';
+        }
+        return null;
+      },
     );
   }
 
   Widget _crearPrecio() {
     return TextFormField(
+      initialValue: producto.valor.toString(),
       keyboardType: TextInputType.numberWithOptions(decimal: true),
       decoration: InputDecoration(
         labelText: 'Precio'
       ),
+      onSaved: (value) => producto.valor = double.parse(value),
+      validator: (value) {
+        if (utils.isNumeric(value)) {
+          return null;
+        }
+        return 'Sólo se permiten números';
+      },
     );
   }
 
@@ -61,7 +88,28 @@ class ProductoPage extends StatelessWidget {
       textColor: Colors.white,
       label: Text('Guardar'),
       icon: Icon(Icons.save),
-      onPressed: () {},
+      onPressed: _submit,
     );
+  }
+
+  Widget _crearDisponible(BuildContext context) {
+    return SwitchListTile(
+      value: producto.disponible,
+      title: Text('Disponible'),
+      activeColor: Theme.of(context).primaryColor,
+      onChanged: (value) => setState(() {
+        producto.disponible = value;
+      }),
+    );
+  }
+
+  void _submit() {
+    if (!formKey.currentState.validate()) return;
+
+    formKey.currentState.save();
+
+    print(producto.titulo);
+    print(producto.valor);
+    print(producto.disponible);
   }
 }
